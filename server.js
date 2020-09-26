72e7d9bf500d0dbea80746f89dbfd5e8b83a1cf6
invierno <1354002980@qq.com>
2020-09-26 08:27:58 +0800

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require("url");
 var port = process.argv[2];
 
 if (!port) {
-  console.log("请指定端口号好不啦？\nnode server.js 8888 这样不会吗？");
+  console.log("请指定端口号，例如\nnode server.js 8888");
   process.exit(1);
 }
 
@@ -21,17 +21,30 @@ var server = http.createServer(function (request, response) {
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
+  console.log("接收到请求！路径（带查询参数）为：" + pathWithQuery);
 
   if (path === "/") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(`二哈`);
+
+    response.write(`
+    <link rel="stylesheet" href="./style.css">
+    <!DOCTYPE html>
+    <html lang="zh-CN">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        <h1>Hello World!</h1>
+      </body>
+    </html>`);
     response.end();
-  } else if (path === "/x") {
+  } else if (path === "/style.css") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/css;charset=utf-8");
-    response.write(`body{color: red;}`);
+    response.write(`h1{color: red}`);
     response.end();
   } else {
     response.statusCode = 404;
